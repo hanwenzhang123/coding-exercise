76ad8b95d9021c13a0f10c48907a0c31f56bf77f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-29 18:35:47 -0400

    Update return-average.js

--- javascript/return-average.js
+++ javascript/return-average.js
@@ -7,3 +7,21 @@ function average(scores){
 average = total/scores.length;
 return(average);
 };
+
+
+
+function average(arg){
+    var total = 0;
+    for(var i = 0; i < arg.length; i++){
+       total += arg[i];
+    }
+    var avg = total / arg.length;
+    console.log(Math.ceil(avg));
+
+}
+
+var scores = [90, 98, 89, 100, 100, 86, 94]; average(scores); //should return 94
+
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49]; average(scores2); //should return 68
+
+
