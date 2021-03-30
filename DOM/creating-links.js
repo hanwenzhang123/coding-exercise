6f7345f21707f8ac127d2f194dccec8414677af8
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-03-30 10:21:37 -0400

    Create creating-links.js

---
+++ DOM/creating-links.js
@@ -0,0 +1,60 @@
+//This code should create a sequence of div elements that each hold a headline above and a link below. 
+
+const languages = ['Python', 'JavaScript', 'PHP', 'Ruby', 'Java', 'C'];
+const section = document.getElementsByTagName('section')[0];
+
+// Accepts a language name and returns a wikipedia link
+function linkify(language) {
+  const a = document.createElement('a');
+  const url = 'https://wikipedia.org/wiki/' + language + '_(programming_language)';
+  a.href = url;
+  return a;
+}
+
+// Creates and returns a div
+function createDiv(language) {
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+  const link = linkify(language);
+  
+  h2.textContent = language;
+  p.textContent = 'Information about ' + language;
+  link.appendChild(p);
+  
+  div.appendChild(h2);
+  
+  // Your code below
+
+  
+  // end
+  
+  return div;
+}
+
+for (let i = 0; i < languages.length; i += 1) {
+  let div = createDiv(languages[i]);
+  
+  section.appendChild(div);
+}
+
+
+
+
+//HTML Code
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Programming Languages</title>
+  <style>
+    body {
+      font-family: sans-serif;
+    }
+  </style>
+</head>
+<body>
+  <h1>Programming Languages</h1>
+  <section></section>
+  <script src="app.js"></script>
+</body>
+</html>
