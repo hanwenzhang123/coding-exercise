19fd33f5afd5d3e59d72508f1664be2ee24f4934
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-02 14:56:08 -0400

    Update college-level.js

--- object-oriented-js/college-level.js
+++ object-oriented-js/college-level.js
@@ -1,11 +1,23 @@
-  get level () {
-    if (this.credits >= 90) {
-      return 'Senior';
-    } else if (this.credits <= 90 && this.credits >= 61) {
-      return 'Junior';
-    } else if (this.credits <= 60 && this.credits <= 31) {
-      return 'Sophomore';
-    } else if (this.credits <= 30) {
-      return 'Freshman';
+class Student {
+    constructor(gpa, credits){
+        this.gpa = gpa;
+        this.credits = credits;
     }
+
+    stringGPA() {
+        return this.gpa.toString();
+    }
+  get level (){
+        let credits = this.credits;
+
+        if (credits > 90) {return 'Senior';}
+
+        else if (credits <= 90 && credits >= 61 ) {return 'Junior';}
+
+        else if (credits <= 60 && credits >= 31) {return 'Sophomore';}
+
+        else if (credits <= 30 ) {return 'Freshman';}
   }
+}
+
+const student = new Student(3.9);
