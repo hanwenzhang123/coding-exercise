d19653c172f8398013d0e085c40327dd5657f2d2
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-03 11:48:53 -0400

    Create abbreviated-days.js

---
+++ javascript/abbreviated-days.js
@@ -0,0 +1,9 @@
+//Use the map method on the daysOfWeek array, creating a new array of abbreviated week days. 
+//Each abbreviated string should be the first three letters of the long version in daysOfWeek. 
+//Store the new array in the variable abbreviatedDays.
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let abbreviatedDays = daysOfWeek.map(days => days.slice(0, 3));
+
+// abbreviatedDays should be: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+// Write your code below
