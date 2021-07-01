4ef1bd9bcc9731c2021022659ac0a32435a8f84f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-01 11:49:57 -0400

    Update 01-temperature.cpp

--- c-plus-plus/01-temperature.cpp
+++ c-plus-plus/01-temperature.cpp
@@ -5,13 +5,33 @@ The formula is the following:
 C = (F - 32) / 1.8C=(F−32)/1.8
 
 
-
+//1
 #include <iostream>
 
 int main() {
   
   double tempf = 83.0;
   double tempc;
+  
+  tempc = (tempf - 32) / 1.8;
+  
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
+}
+
+
+//2
+#include <iostream>
+
+int main() {
+  
+  double tempf;
+  double tempc;
+  
+  // Ask the user
+  std::cout << "Enter the temperature in Fahrenheit: ";
+  
+  std::cin >> tempf;
   
   tempc = (tempf - 32) / 1.8;
   
